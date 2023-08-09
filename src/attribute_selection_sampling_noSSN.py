09c46a1cced520e0fb6cfc7122cc2e2da8435f39
Nachiket18 <nachideo.em@gmail.com>
2023-08-08 20:53:42 -0400

    Adding comments to function used in sampling,parsing_xmland config file

--- src/attribute_selection_sampling_noSSN.py
+++ src/attribute_selection_sampling_noSSN.py
@@ -8,7 +8,7 @@ with open('../config.yaml', 'r') as f:
 
 
 BLOCKING_ATTRIBUTE = config["BLOCKING_ATTRIBUTE"]
-## 3 - Last Name for Pseduopeople
+# 3 - Last Name for Pseduopeople
 
 TOTAL_RATE = config["TOTAL_RATE"]
 THRESHOLD = config["THRESHOLD"]
@@ -27,93 +27,128 @@ output_records_sample = []
 # dict{0} --> 0
 # dict{1} --> 3
 
+"""
+
+FUNCTION de_duplication
+INPUT: dataset (Pandas DataFrame)
+OUTPUT: de-duplicate Dataset (that contains unique records) and a dictionary with information about record 
+in de-duplicate dataset and it's location in the original dataset.
+information about the location is needed later to find exact matches.
+
+"""
+
 
 def de_duplication(dataset: pl.DataFrame):
-    dataset_pand = dataset.to_pandas() ## Converts to pandas dataframe
-    dataset_pand_lst = dataset_pand.values.tolist() ## Converts to list of list
+    dataset_pand = dataset.to_pandas()  # Converts to pandas dataframe
+    dataset_pand_lst = dataset_pand.values.tolist()  # Converts to list of list
     index_original_df = {}
-    de_duplicate = [dataset_pand_lst[0]] ## adds first element to de duplicated data
+    # adds first element to de duplicated data
+    de_duplicate = [dataset_pand_lst[0]]
     index_original_df[0] = 0
 
     # ['Rany','Kamel'], ['Rany','Kaml']
     #
 
-    for i in range(1,len(dataset_pand_lst)):
+    for i in range(1, len(dataset_pand_lst)):
         if dataset_pand_lst[i] != dataset_pand_lst[i-1]:
             de_duplicate.append(dataset_pand_lst[i])
             len_d = len(de_duplicate)
             index_original_df[len_d-1] = i
         else:
             continue
-    
+
     for i in range(len(de_duplicate)):
         de_duplicate[i] = [str(x).lower() for x in de_duplicate[i]]
 
-
-    #pl_de_duplicate = pd.DataFrame(de_duplicate)
+    # pl_de_duplicate = pd.DataFrame(de_duplicate)
     # print("De-duplicate",de_duplicate[1])
     # print("Original",dataset_pand_lst[0:5])
     # print(index_original_df[1])
     # print(pl_de_duplicate)
-    return de_duplicate,index_original_df
+    return de_duplicate, index_original_df
+
+
+"""
+
+FUNCTION: do_blocking
+INPUT: de-duplicate dataset (list)
+OUTPUT: DOES NOT RETURN ANYTHING but fills the information about records in the respective blocks.
+Uses K-mer blocking.
+
+REFERS: generate_k_mer()
+
+"""
+
 
 def do_blocking(de_duplicated_set: list):
 
     for i in range(len(de_duplicated_set)):
         tmp = de_duplicated_set[i]
-        k_mer_list = generate_k_mer(tmp[BLOCKING_ATTRIBUTE],3)
-        
+        k_mer_list = generate_k_mer(tmp[BLOCKING_ATTRIBUTE], 3)
+
         for asc in k_mer_list:
-           if asc.lower() in dict_blocks:
+            if asc.lower() in dict_blocks:
                 dict_blocks[asc.lower()].append(i)
-           else:
+            else:
                 dict_blocks[asc.lower()] = [i]
 
-def generate_k_mer(str_d, k:int) -> list:
+
+"""
+
+FUNCTION generate_k_mer
+INPUT: A single String, k
+
+"""
+
+
+def generate_k_mer(str_d, k: int) -> list:
     if len(str_d) <= k:
         return [str_d]
 
-    return [str_d[i:i+k] for i in range(0,len(str_d)-(k-1))]
+    return [str_d[i:i+k] for i in range(0, len(str_d)-(k-1))]
 
 
+def generatePairs(df_sorted: pl.DataFrame, index_duplicate: dict, key: int):
 
-def generatePairs(df_sorted: pl.DataFrame, index_duplicate:dict, key:int):
-    
     output_records_sample.append(df_sorted[index_duplicate[key]])
     if (key + 1 < len(index_duplicate)) and (index_duplicate[key] != index_duplicate[key+1] - 1):
         output_records_sample.append(df_sorted[index_duplicate[key] + 1])
-    
-    
-##  Original [Rany,Kamel], [Rany,Kamel]  [Nachiket,Deo] , [Nachiket Deo]   
-##  De-duplicate [Rany,Kamel] [Nachiket,Deo]
+
+
+# Original [Rany,Kamel], [Rany,Kamel]  [Nachiket,Deo] , [Nachiket Deo]
+# De-duplicate [Rany,Kamel] [Nachiket,Deo]
     # key = 0 [Rany Kamel]
     # Key + 1 = 2 --> [Nachiket Deo]
 
 ##
-## TO BE IMPLEMENTED BY RANY
+# TO BE IMPLEMENTED BY RANY
 ##
 
 
-def random_data_generation(df_sorted: pl.DataFrame, de_duplicate:list, index_duplicate: dict, total_rate: float = TOTAL_RATE) -> None:
-    
-    ## Generate Random records from de_duplicate
-    ## Total number of records to be generated are total_number of records (de_duplicate) * TOTAL_RATE
-    ## Find the Exact associated records and push them to output (Call generatePairs function for this)
+"""
 
-    ## Random record --> take it's index
-    ## Generate K-mer of blocking attribute
-    ## Query in dict_blocks and get indexes
-    ## Pass the indexes to generatePairs()
-    ## [Rany,Kamel] (1) ... [Rane,Kamel] (10)
-    ## Kam,Ame,Mel
-    # dict_blocks[Kam] = [1,10..]
-    # dict_blocks[Ame] = [1,10..]
-    # dict_blocks[Mel] = [1,10..]
+FUNCTION random_data_generation
+INPUT: df_sorted (dataframe containing original data in sorted order),de_duplicate(The list containing unique elements),index_duplicate(Dictionary containing information about location of records in de_duplicate inside original dataset),total_rate(Sampling rate)
+OUTPUT: DOES NOT RETURN ANYTHING but generates sample of the original dataset based on the sampling rate.
+REFERS: generate_k_mer(),generatePairs()
 
-    # get random sample of indexes
-    samp = random.sample(range(0, len(de_duplicate)), int(len(de_duplicate) * total_rate))
+"""
 
 
+def random_data_generation(df_sorted: pl.DataFrame, de_duplicate: list, index_duplicate: dict, total_rate: float = TOTAL_RATE) -> None:
+    """
+
+        1.Generate Random records from de_duplicate
+        2.Total number of records to be generated are total_number of records (de_duplicate) * TOTAL_RATE
+        3.For every randomly generated record, find it's k-mers and the blocks associated to it.
+        4.Go through every other record in the block from previous step and find the levenshtein distance betweehn them
+        5.If the distance between records is less than or equal to threshold then add them to output.
+        6.Find the Exact associated records (Records with no errors) and push them to output (Call generatePairs function for this)
+
+    """
+
+    samp = random.sample(range(0, len(de_duplicate)),
+                         int(len(de_duplicate) * total_rate))
 
     for i in range(len(samp)):
         print("Generating k-mers...", f"{i}/{len(samp)}", "\t" * 5, end='\r')
@@ -123,49 +158,57 @@ def random_data_generation(df_sorted: pl.DataFrame, de_duplicate:list, index_dup
 
         for kmer in k_mer_list:
             if kmer in dict_blocks:
-                #print(kmer)
+                # print(kmer)
                 kmer_index = kmer_index.union(dict_blocks[kmer.lower()])
 
-        #print(kmer_index)
-
         # print("Generating pairs...")
-        
+
         for index in kmer_index:
-            #generatePairs(df_sorted, index_duplicate, index)
+            # generatePairs(df_sorted, index_duplicate, index)
             tmp_record = de_duplicate[index]
             l_dist = 0
-            
-            for i in range(1,len(tmp_record)):
+
+            for i in range(1, len(tmp_record)):
                 a = tmp_record[i]
                 b = record[i]
 
-                l_dist += Levenshtein.distance(a,b)
-                
+                l_dist += Levenshtein.distance(a, b)
+
                 if l_dist > THRESHOLD:
                     l_dist = THRESHOLD + 1
                     break
 
             if l_dist <= THRESHOLD:
                 generatePairs(df_sorted, index_duplicate, index)
-            
 
-def write_output(_sep = "\t", sample_ds_1 = "sample_ds_1.txt", sample_ds_2 = "sample_ds_2.txt", headers = []):
-    _flip = 0 # 0 for ds_1, 1 for ds_2
-    flip_cond = lambda : _flip % 2
 
+"""
+
+FUNCTION write_output
+INPUT: sample_ds_1(String,file_name for output_1),sample_ds_2(String,file_name for output_2)
+OUTPUT:Writes the record of the Sample to output
+Two output paths are needed as dataset is split into two files (Necessary condition for running the RLA code)
+
+"""
+
+
+def write_output(_sep="\t", sample_ds_1="sample_ds_1.txt", sample_ds_2="sample_ds_2.txt", headers=[]):
+    _flip = 0  # 0 for ds_1, 1 for ds_2
+    def flip_cond(): return _flip % 2
 
     header = _sep.join(headers) + "\n"
 
-    with open(sample_ds_1, 'w',encoding = "utf-8") as f1, open(sample_ds_2, 'w',encoding = "utf-8") as f2:
+    with open(sample_ds_1, 'w', encoding="utf-8") as f1, open(sample_ds_2, 'w', encoding="utf-8") as f2:
         f1.write(header)
         f2.write(header)
 
         for record in output_records_sample:
-            rec = lambda ind : str(record[ind].item())
-            #record_csv = _sep.join([rec("SSN") , rec("Last_Name"), rec("First_Name"), rec("DOB"), rec("DOD")]) + "\n"
-            record_csv = _sep.join([rec("simulant_id") , rec("first_name"), rec("middle_initial"), rec("last_name"), rec("age"), rec("date_of_birth"), rec("street_number"), rec("street_name"), rec("unit_number"), rec("city"), rec("state"), rec("zipcode"), rec("relation_to_reference_person"), rec("sex"), rec("race_ethnicity")]) + "\n"
+            def rec(ind): return str(record[ind].item())
+            # record_csv = _sep.join([rec("SSN") , rec("Last_Name"), rec("First_Name"), rec("DOB"), rec("DOD")]) + "\n"
+            record_csv = _sep.join([rec("simulant_id"), rec("first_name"), rec("middle_initial"), rec("last_name"), rec("age"), rec("date_of_birth"), rec("street_number"), rec(
+                "street_name"), rec("unit_number"), rec("city"), rec("state"), rec("zipcode"), rec("relation_to_reference_person"), rec("sex"), rec("race_ethnicity")]) + "\n"
 
-            #print(record_csv)
+            # print(record_csv)
             if flip_cond():
                 f1.write(record_csv)
             else:
@@ -173,10 +216,7 @@ def write_output(_sep = "\t", sample_ds_1 = "sample_ds_1.txt", sample_ds_2 = "sa
             _flip += 1
 
 
-# AALTO     ┆ ROBERT     ┆ 6222003 ┆ 5091963
-# aaltop    ┆ robert     ┆ 6222003 ┆ 5091963
-
-def time_code(df: pl.DataFrame, tr_samps: list, out_file_1 = "./data/pse_sample.1.1", out_file_2 = "./data/pse_sample.1.2") -> list:
+def time_code(df: pl.DataFrame, tr_samps: list, out_file_1="./data/pse_sample.1.1", out_file_2="./data/pse_sample.1.2") -> list:
     import time
 
     times = []
@@ -186,7 +226,7 @@ def time_code(df: pl.DataFrame, tr_samps: list, out_file_1 = "./data/pse_sample.
         output_records_sample.clear()
 
         start = time.time()
-        
+
         de_duplicate, index_duplicate = de_duplication(df)
         do_blocking(de_duplicated_set=de_duplicate)
 
@@ -197,9 +237,10 @@ def time_code(df: pl.DataFrame, tr_samps: list, out_file_1 = "./data/pse_sample.
 
         print(f"Time taken: {times[-1]} seconds")
 
-        write_output(sample_ds_1 = out_file_1, sample_ds_2 = out_file_2)
+        write_output(sample_ds_1=out_file_1, sample_ds_2=out_file_2)
 
     return times
+
 
 def attribute_sampling(df: pl.DataFrame, output_files: str | list = "./out.1"):
 
@@ -214,15 +255,23 @@ def attribute_sampling(df: pl.DataFrame, output_files: str | list = "./out.1"):
     else:
         raise TypeError("output_files must be a string or a list of strings")
 
-    times = time_code(df, tr_samp, out_file_1 = out1, out_file_2 = out2)
+    times = time_code(df, tr_samp, out_file_1=out1, out_file_2=out2)
 
     return times
-    
 
 
-def _plot(times, tr_samples, fig_name = "./fig.png", xticks = None, yticks = None):
+"""
+
+NOT USED
+WAS NEEDED TO GENERATE PLOTS FOR RESEARCH PAPER
+
+"""
+
+
+def _plot(times, tr_samples, fig_name="./fig.png", xticks=None, yticks=None):
     import matplotlib.pyplot as plt
-    plt.plot(times, [int(x*100) for x in tr_samples], marker='o', linestyle='--')
+    plt.plot(times, [int(x*100)
+             for x in tr_samples], marker='o', linestyle='--')
     plt.xlabel("Run Time (s)")
     plt.ylabel("Total Rate (%)")
 
@@ -236,19 +285,10 @@ def _plot(times, tr_samples, fig_name = "./fig.png", xticks = None, yticks = Non
     plt.savefig(fig_name)
     plt.show()
 
-    
 
 if __name__ == "__main__":
-    _plot(*attribute_sampling(), xticks = [5500,6000,6500,7000,7500,8000,8500], yticks = [2,3,4,5,6])
+    _plot(*attribute_sampling(),
+          xticks=[5500, 6000, 6500, 7000, 7500, 8000, 8500], yticks=[2, 3, 4, 5, 6])
 
 
-
-
-
-
-
-
-
-
-
-#print(df_SSN)
+# print(df_SSN)
--- src/feature_selection_data_prune.py
+++ src/feature_selection_data_prune.py
@@ -1,5 +1,17 @@
 from math import ceil
 
+"""
+
+FUNCTION prune_feature_data
+INPUT: path (Path to input file),output(path for output file), keep (Amount of duplicate mismatched record to keep)
+OUTPUT: Pruned processed data file.
+
+LOGIC:
+    1. Go through and keep track of number of matched and mismatched records in two seperate dictionaries.
+    2. Write the matched records without pruning.
+    3. Prune the mismatched records as per keep paramater and then write to output.
+
+"""
 def prune_feature_data(path: str, output: str, keep: float = 0.20):
     with open(path, "r") as f:
         data = f.readlines()
@@ -8,7 +20,7 @@ def prune_feature_data(path: str, output: str, keep: float = 0.20):
         features_keep = {}
 
         for d in data:
-            if int(d[-2]) == 0: # Gets last character, if mismatch
+            if int(d[-2]) == 0:  # Gets last character, if mismatch
                 if d in features_prune:
                     features_prune[d] += 1
                 else:
@@ -18,22 +30,22 @@ def prune_feature_data(path: str, output: str, keep: float = 0.20):
                     features_keep[d] += 1
                 else:
                     features_keep[d] = 1
-                
-    
+
     with open(output, "w") as f:
         for d, count in features_keep.items():
             for _ in range(count):
                 f.write(d)
-        
+
         for d, count in features_prune.items():
             prune_perc = ceil(count * keep)
-            #print(prune_perc, count)
             for _ in range(prune_perc):
                 f.write(d)
 
 
-
 if __name__ == "__main__":
-    prune_feature_data(path = "../data/feature_selection_processed_data_file_pse_3.csv", output = "../data/fs_pruned_data_pse_3.csv", keep = .2)
-    prune_feature_data(path = "../data/feature_selection_processed_data_file_pse_4.csv", output = "../data/fs_pruned_data_pse_4.csv", keep = .2)
-    prune_feature_data(path = "../data/feature_selection_processed_data_file_pse_5.csv", output = "../data/fs_pruned_data_pse_5.csv", keep = .2)
+    prune_feature_data(path="../data/feature_selection_processed_data_file_pse_3.csv",
+                       output="../data/fs_pruned_data_pse_3.csv", keep=.2)
+    prune_feature_data(path="../data/feature_selection_processed_data_file_pse_4.csv",
+                       output="../data/fs_pruned_data_pse_4.csv", keep=.2)
+    prune_feature_data(path="../data/feature_selection_processed_data_file_pse_5.csv",
+                       output="../data/fs_pruned_data_pse_5.csv", keep=.2)
