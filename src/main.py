7d4538461a847c7f4378340a2a898dc0a8c2b6ca
Nachiket18 <nachideo.em@gmail.com>
2023-08-09 10:24:10 -0400

    Formated the document with autopep8

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 import yaml
 import polars as pl
 from attribute_selection_sampling_noSSN import attribute_sampling
-import os, subprocess
+import os
+import subprocess
 import pandas as pd
 from parse_xml import parse_xml
 from feature_selection_data_prune import prune_feature_data
@@ -11,10 +12,10 @@ with open('../config.yaml', 'r') as f:
     config = yaml.safe_load(f)
 
 
-#attribute_selection_nosnn -> 
-#C++ for generating processed dataset -> 
-#feature_selection_data_prune -> 
-#apriori.py
+# attribute_selection_nosnn ->
+# C++ for generating processed dataset ->
+# feature_selection_data_prune ->
+# apriori.py
 
 RLA_DIR = "../RLA_CL_EXTRACT/"
 
@@ -29,38 +30,45 @@ def sample_generation():
     df1_file = config["input_files"]["input_file_1"]
     df2_file = config["input_files"]["input_file_2"]
 
-    df1 = pl.read_csv(df1_file, separator = '\t', has_header = False, new_columns = col_names,
-                   dtypes={x: pl.Utf8 for x in col_names})
-    df2 = pl.read_csv(df2_file, separator = '\t', has_header = False, new_columns = col_names,
-                   dtypes={x: pl.Utf8 for x in col_names})
-    
+    df1 = pl.read_csv(df1_file, separator='\t', has_header=False, new_columns=col_names,
+                      dtypes={x: pl.Utf8 for x in col_names})
+    df2 = pl.read_csv(df2_file, separator='\t', has_header=False, new_columns=col_names,
+                      dtypes={x: pl.Utf8 for x in col_names})
+
     ''' Formatting Data '''
     df_merged = pl.concat([df1, df2])
-        #df_sorted = df_merged.sort(by=['Last_Name','First_Name','DOB','DOD'],descending=False)
-    df_sorted = df_merged.sort(by = cols_wo_id, descending = False)
+    # df_sorted = df_merged.sort(by=['Last_Name','First_Name','DOB','DOD'],descending=False)
+    df_sorted = df_merged.sort(by=cols_wo_id, descending=False)
 
-
-    attribute_sampling(df_sorted, output_files = list(config["sample_output"].values()))
+    attribute_sampling(df_sorted, output_files=list(
+        config["sample_output"].values()))
 
 
 def call_RLA():
     parse_xml()
 
-    sample_data = pd.read_csv(config["input_files"]["input_file_1"], sep = '\t', header = None)
+    sample_data = pd.read_csv(
+        config["input_files"]["input_file_1"], sep='\t', header=None)
 
-    generate_processed_data = subprocess.check_call([os.path.join(RLA_DIR, "bin/rlacl")] + [config["rla_xml_file"], str(len(sample_data)), "0","0","0","0"])
+    generate_processed_data = subprocess.check_call([os.path.join(
+        RLA_DIR, "bin/rlacl")] + [config["rla_xml_file"], str(len(sample_data)), "0", "0", "0", "0"])
 
-    subprocess.run(["mv", os.path.join(RLA_DIR, "feature_selection_processed_data_file.csv"), "../data"])
+    subprocess.run(["mv", os.path.join(
+        RLA_DIR, "feature_selection_processed_data_file.csv"), "../data"])
+
 
 def feature_prune():
-    prune_feature_data(path = "../data/feature_selection_processed_data_file.csv", output = config["pruned_output"], keep = config["FEATURE_PRUNE_KEEP"])
+    prune_feature_data(path="../data/feature_selection_processed_data_file.csv",
+                       output=config["pruned_output"], keep=config["FEATURE_PRUNE_KEEP"])
+
 
 def apriori():
-    run_apriori(config["pruned_output"], output_file = config["rules_output"])
+    run_apriori(config["pruned_output"], output_file=config["rules_output"])
+
 
 if __name__ == "__main__":
-    #call_RLA(*list(config["sample_output"].values()))
-    #exit()
+    # call_RLA(*list(config["sample_output"].values()))
+    # exit()
 
     if config["run"]["sample_generation"]:
         print("Running attribute selection sampling...\n")
