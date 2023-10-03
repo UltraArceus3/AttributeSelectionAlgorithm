2a937a59eac15f253aa78eba8dea635bcabafcac
Nachiket18 <nachideo.em@gmail.com>
2023-10-03 16:32:11 -0400

    Adding the setup file for cython with C++

--- src/setup.py
+++ src/setup.py
@@ -2,5 +2,5 @@ from distutils.core import Extension, setup
 from Cython.Build import cythonize
 
 
-ext = Extension(name="edge_generation", sources=["edge_generation.pyx"])
+ext = Extension(name="edge_generation", sources=["edge_generation.pyx"],extra_compile_args=["-O3","-fopenmp"],extra_link_args=['-fopenmp'],language="c++")
 setup(ext_modules=cythonize(ext))
